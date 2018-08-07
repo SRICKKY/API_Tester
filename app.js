a89a29363b042fdb9ce00d5c2a2d89eb73254a65
Srikant <kumar_srikant@outlook.com>
2018-08-08 03:33:11 +0530

    url updated with query string & headers passed as object!

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ document.getElementById('add-query').addEventListener('click',addQueryInputField
 function addQueryInputField(){
     query = `
         <div id="query-item" class="add-margin-top">
-            <div>
+            <div id="query_container">
                 <input type="text" placeholder="Name" class="col-4">&nbsp;=
                 <input type="text" placeholder="Value">
                 &nbsp;<span class="x-symbol" id="query_remove_button" onclick="removeQueryInputField()">&times;</span>
@@ -63,28 +63,72 @@ function removeQueryInputField(){
     // document.getElementById('query-item').innerHTML = '';
 }
 
-
 function pullEndpoint(){
+
     method = document.getElementById('method').value;
-    url = document.getElementById('url').value;
     output = document.getElementById('output-box');
     output.innerHTML = '';
-    
+    url = document.getElementById('url').value;
+
+    // Check at URL end whether it's ending with / or not?
+    if(url.charAt(url.length - 1) != "/"){
+        url += "/"
+    }
+
     if(method == "POST"){
-        data = '';
-        url += `?{data}`
+
+        // Grabing Data from Header Input Fields
+        header_li = []
+
+        dict = {}
+        headers_el = document.querySelectorAll('#header-item')
+
+        for(i=0; i<headers_el.length; i++){
+            for(j=0; j<2; j++){
+                header_li.push(document.querySelectorAll('#header-item')[i].children[j].value);
+            }
+        }
+
+        for(i=0; header_li.length; i++){
+            dict[header_li[i]] = `${header_li[++i]}`;
+        }
+
+        //////////////////////
+
+
+        // Query String
+
+
+        query_list = []
+
+        for(i=0; i<document.querySelectorAll('#query_container').length; i++){
+            for(j=0; j<2; j++){
+                query_list.push(document.querySelectorAll('#query_container')[i].children[j].value)
+            }
+        }
+
+        main_query = ""
+
+        for(i=0; i<query_list.length; i++){
+            main_query += query_list[i]
+            main_query += "="
+            main_query += query_list[++i]
+            main_query += "&"
+        }
+        // Query String Ends Here
+
+        // Modifying URL by adding query fields into the main URL
+        url += `?${main_query}`
+        
         fetch(url, {
             method: method, // *GET, POST, PUT, DELETE, etc.
             // mode: "cors", // no-cors, cors, *same-origin
             cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
             // credentials: "same-origin", // include, same-origin, *omit
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-                // "Content-Type": "application/x-www-form-urlencoded",
-            },
+            headers: dict,
             redirect: "follow", // manual, *follow, error
             referrer: "no-referrer", // no-referrer, *client
-            body: JSON.stringify(data), // body data type must match "Content-Type" header
+            // body: JSON.stringify(data), // body data type must match "Content-Type" header
         })
         .then(function(response){
             document.getElementById('status-code').innerHTML = `Response Code: ${response.status}`;
@@ -115,16 +159,53 @@ function pullEndpoint(){
         .catch(err => console.log(err));
     }
     if(method == "PUT"){
+        // Grabing Data from Header Input Fields
+        header_li = []
+
+        dict = {}
+        headers_el = document.querySelectorAll('#header-item')
+
+        for(i=0; i<headers_el.length; i++){
+            for(j=0; j<2; j++){
+                header_li.push(document.querySelectorAll('#header-item')[i].children[j].value);
+            }
+        }
+
+        for(i=0; header_li.length; i++){
+            dict[header_li[i]] = `${header_li[++i]}`;
+        }
+
+        //////////////////////
+
+
+        // Query String
+
+
+        query_list = []
+
+        for(i=0; i<document.querySelectorAll('#query_container').length; i++){
+            for(j=0; j<2; j++){
+                query_list.push(document.querySelectorAll('#query_container')[i].children[j].value)
+            }
+        }
+
+        main_query = ""
+
+        for(i=0; i<query_list.length; i++){
+            main_query += query_list[i]
+            main_query += "="
+            main_query += query_list[++i]
+            main_query += "&"
+        }
+        // Query String Ends Here
         fetch(url,{
             method: method,
             // mode: "cors",
             cache: "no-cache",
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-            },
+            headers: dict,
             redirect: "follow",
             referrer: "no-referrer",
-            body: JSON.stringify(data),
+            // body: JSON.stringify(data),
         })
         .then(function(response){
             document.getElementById('status-code').innerHTML = `Response Code: ${response.status}`;
@@ -133,7 +214,7 @@ function pullEndpoint(){
     
         })
         .then(function(data){
-            document.getElementById('output-box').innerHTML = `<p>Info has been UPDATED!`;
+            document.getElementById('output-box').innerHTML = `<p>Info has been UPDATED!</p>`;
             // document.getElementById('output-box').appendChild(
             //     renderjson(data)
             // );
@@ -150,7 +231,7 @@ function pullEndpoint(){
     
         })
         .then(function(data){
-            document.getElementById('output-box').innerHTML = `<p>Info has been DELETED!`;
+            document.getElementById('output-box').innerHTML = `<p>Info has been DELETED!</p>`;
             // document.getElementById('output-box').appendChild(
             //     renderjson(data)
             // );
@@ -159,3 +240,40 @@ function pullEndpoint(){
         .catch(err => console.log(err));
     }
 }
+
+
+// header_li = []
+// dict = {}
+// headers_el = document.querySelectorAll('#header-item')
+
+// for(i=0; i<headers_el.length; i++){
+//     for(j=0; j<2; j++){
+//         header_li.push(document.querySelectorAll('#header-item')[i].children[j].value);
+//     }
+// }
+
+// for(i=0; header_li.length; i++){
+//     dict[header_li[i]] = `${header_li[++i]}`;
+// }
+
+
+//////////////////////////////////////////////////////////
+
+// / Query String
+
+// query_list = []
+
+// for(i=0; i<document.querySelectorAll('#query_container').length; i++){
+// 	for(j=0; j<2; j++){
+// 		query_list.push(document.querySelectorAll('#query_container')[i].children[j].value)
+// 	}
+// }
+
+// main_query = ""
+
+// for(i=0; i<query_list.length; i++){
+// 	main_query += query_list[i]
+// 	main_query += "="
+// 	main_query += query_list[++i]
+// 	main_query += "&"
+// }
